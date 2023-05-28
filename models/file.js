cc0c62a43674bd36fba1defb0e0014a6fa92fe96
piyushbhawsar <111758968+piyushbhawsar@users.noreply.github.com>
2023-05-28 23:16:41 +0530

    feat. Email upon Successfull Upload

--- models/file.js
+++ models/file.js
@@ -1,4 +1,6 @@
 const { default: mongoose } = require("mongoose");
+const nodemailer = require("nodemailer")
+require("dotenv").config()
 
 const fileSchema = mongoose.Schema({
     name: {
@@ -14,6 +16,27 @@ const fileSchema = mongoose.Schema({
     email: {
         type: String,
     },
+})
+
+fileSchema.post("save", async(doc) => {
+    try{
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST ,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS 
+            }
+        })
+    let info = await transporter.sendMail({
+        from: "Bhawsar Baba" ,
+        to: doc.email,
+        subject: "Mail Sent From Backend(testing...)",
+        html: `<h2>Hello Jee</h2> <p>File Uploaded jee</p> View Here: <a href="${doc.imageUrl}">${doc.imageUrl}</a>` 
+    })
+    } catch(e) {
+
+        
+    }
 })
 
 module.exports = mongoose.model("File", fileSchema)
